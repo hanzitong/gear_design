1d83df152af102859c530c2ff936fb04670890e4
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2024-07-22 14:18:17 +0900

    add calc_sigbase_f_gear_y.cpp

---
+++ src/calc_sigbase_f_gear_y.cpp
@@ -0,0 +1,37 @@
+
+#include <iostrem>
+#include <vector>
+#include <nlopt.hpp>
+
+#include "../../include/gear_design/f_gear.hpp"
+
+
+// double obj_gearprofile1_gearprofile2_intersection(const double& x, void* chank) {
+//     GearParamQuadratic* params = static_cast<GearParamQuadratic>(chank);
+//     const double radius = params -> radius;
+//     const double theta = params -> theta;
+    
+double obj_gearprofile1_gearprofile2_intersection(
+                                                  const double& x_gear,     // variable (to be optimized)
+                                                  const double& y_base,     // variable (to be optimized)
+                                                  const double& x_base,     // input (known)
+                                                  const double& radius,     // constant
+                                                  const double& theta) {    // constant
+    double cos = std::cos(theta);
+    double sin = std::sin(theta);
+    double y_base = siggear_f_gearprofile(x_gear);
+    double res = x_base * (cos + sin) - y_base * (sin + cos) - radius * sin + radius * cos - x_base - y_base;
+
+
+    return res;
+}
+
+
+double calc_gearprofile1_gearprofile2_intersection(double x_base, double radius, double theta) {
+    
+
+
+
+    return -1;
+}
+
