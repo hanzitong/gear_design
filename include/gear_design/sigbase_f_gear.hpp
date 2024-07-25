c8a0354b075db1f4071647f079fdefb823bb2658
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2024-07-25 16:37:56 +0900

    periodical update (passed compiling gendata_objfunc.cpp

--- include/gear_design/sigbase_f_gear.hpp
+++ include/gear_design/sigbase_f_gear.hpp
@@ -1,5 +1,6 @@
 
 
+
 #ifndef SIGBASE_F_GEAR_HPP
 #define SIGBASE_F_GEAR_HPP
 
@@ -7,15 +8,9 @@
 
 namespace gear_design{
 
-double obj_gearprofile1_gearprofile2_intersection(
-                                                  const double& x_gear,     // variable (to be optimized)
-                                                  const double& y_base,     // variable (to be optimized)
-                                                  const double& x_base,     // input (known)
-                                                  const double& radius,     // constant
-                                                  const double& theta);     // constant
  
 double obj_gearprofile1_gearprofile2_intersection_nlopt(
-                                                        const double& x_base,
+                                                        const std::vector<double>& x_base,
                                                         std::vector<double>& grad, 
                                                         void* f_data);
  
--- plot/f_gear/gendata_objfunc.cpp
+++ plot/f_gear/gendata_objfunc.cpp
@@ -11,26 +11,38 @@
 
 
 int main() {
-    /* prepare the plot range of x-axis */
+    /* basic declaration */
+    std::vector<double> x_data(1000, 0.);
+    std::vector<double> y_data(1000, 0.);
     std::vector<double> dammy(10, 0.);
+
+    /* prepare the plot range of x-axis */
     double radius = 14.;
     double theta = 100. / 180 * M_PI;
-    gear_design::GearParamFgear params(radius, theta, 0., 0.);
-    std::vector<double> x_vec(1000, 0.);
-    std::vector<double> y_vec(1000, 0.);
     double range_start = -50.;
     double range_delta = 0.5;
-    for (int i = 0; i < x_vec.size(); ++i) {
-        x_vec[i] = range_start + range_delta * i;
-        y_vec[i] = \
-            obj_gearprofile1_gearprofile2_intersection_nlopt(x_vec[i], dammy, (void*)&params);
+
+    /* prepare parameters which objective function needs */
+    gear_design::GearParamFgear params(radius, theta, 0., 0.);
+    std::vector<double> x_ini = {0., 0.};
+
+    /* set plot range x and calculate y (both are on sigbase) */
+    for (int i = 0; i < x_data.size(); ++i) {
+        x_ini[0] = range_start + range_delta * i;
+        y_data[i] = \
+            // gear_design::obj_gearprofile1_gearprofile2_intersection_nlopt(x_data[i], dammy, (void*)&params);
+            gear_design::obj_gearprofile1_gearprofile2_intersection_nlopt(x_ini, dammy, (void*)&params);
     }
 
-    /* put data to a record file */
+    // for (int i = 0; i < y_data.size(); ++i) {
+    //     y_data[i] = gear_design::obj_gearprofile1_gearprofile2_intersection_nlopt(x_data, dammy, (void*)&params);
+    // }
+
+    /* put x,y-data into a record file */
     std::ofstream file;
-    file.open("data/data_objfunc.dat");
+    file.open("objfunc_shape/data_objfunc.dat");
     for (int i = 0; i < 100; ++i) {
-        file << x_vec[i] << ' ' << y_vec[i] << "\n";
+        file << x_data[i] << ' ' << y_data[i] << "\n";
         // if (i % 10 == 0) file << "\n";
     }
     file.close();
