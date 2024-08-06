cf5227068a7ec02abaf1c70bbb70c33136ae72c9
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2024-08-06 16:31:27 +0900

    update include/

--- include/gear_design/f_gear.hpp
+++ include/gear_design/f_gear.hpp
@@ -9,33 +9,46 @@ namespace gear_design {
 
 
 struct GearParamFgear {
-    double radius;  // mm
-    double theta;   // rad
-    double x_base;  // input for objfunction of 
-
-    // double quad_a;      // maybe, no need
-    // double quad_b;      // maybe, no need
+    double radius;  // [m]
+    double theta;   // [rad]
+    double x_base;  // input for calc_sigbase_y_base()
 
     GearParamFgear(double r, double th, double xbase):
-                       radius(r), theta(th), x_base(xbase) {}
+                       radius(r), theta(th), x_base(xbase)
+    {
+    }
 };
 
 
-double siggear_f_gearprofile(const double& x);
-double calc_sigbase_f_gearprofile_x_coordinate(const double& siggear_x_param, const double& radius, const double& theta);
+double siggear_f_gearprofile(const double& x_gear);
+// double siggear_real_gearprofile(const double& x_gear);       // define it in another file
+
+double calc_sigbase_y_gear_from_xgear(
+    const double& x_base,
+    const double& radius,
+    const double& theta,
+    Eigen::Vector3d* sigbase_vec = nullptr);
+
+
+double calc_sigbase_f_gearprofile_x_coordinate(
+    const double& siggear_x_param,
+    const double& radius,
+    const double& theta);
 double calc_sigbase_f_gearprofile_y_coordinate(
     const double& siggear_f_gearprofile_x_param,
     const double& radius,
     const double& theta);
- 
-// double calc_sigbase_f_gearprofile_x_coordinate(const double& , const double& , const double&);
-// double calc_sigbase_f_gearprofile_y_coordinate(const double& , const double& , const double&);
 
-Eigen::Vector3d
-calc_sigbase_f_gearprofile_vector(const Eigen::Vector3d& siggear_P, Eigen::Vector3d& sigbase_P, const double& radius, const double& theta);
+Eigen::Vector3d calc_sigbase_f_gearprofile_vector(
+    const Eigen::Vector3d& siggear_P,
+    Eigen::Vector3d& sigbase_P,
+    const double& radius,
+    const double& theta);
+
+
+
 
 }       // namespace gear_design
-
 
 #endif  // GEAR_DESIGN_F_GEAR_HPP
 
--- include/gear_design/f_gear_objective_function.hpp
+++ include/gear_design/f_gear_objective_function.hpp
@@ -1,8 +1,8 @@
 
 
 
-#ifndef SIGBASE_F_GEAR_HPP
-#define SIGBASE_F_GEAR_HPP
+#ifndef SIGBASE_F_GEAR_OBJECTIVE_FUNCTION_HPP
+#define SIGBASE_F_GEAR_OBJECTIVE_FUNCTION_HPP
 
 #include <vector>
 
@@ -18,6 +18,11 @@ double calc_sigbase_y_base_objfunc_shape(
                                          const std::vector<double>& opt_vec,
                                          std::vector<double>& grad,
                                          void* f_data);
+
+double obj_calc_sigbase_y_from_xgear_1variable(
+                                               const std::vector<double>& opt_vec,
+                                               std::vector<double>& grad,
+                                               void* f_data);
 
 // double calc_sigbase_y_base_objfunc_shape(
 //                                          double& x_gear,    // variable to optmize 
