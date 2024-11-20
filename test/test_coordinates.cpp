10f907211e865f1f4cfe42be7644731ddcac88eb
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2024-11-20 19:35:03 +0900

    (not finished) refactoring test_coordinates.cpp

--- test/test_coordinates.cpp
+++ test/test_coordinates.cpp
@@ -3,53 +3,104 @@
 #include <cassert>
 #include <cmath>
 #include <gtest/gtest.h>
-#include "../include/gear_design/coordinates.hpp"
+
 #include <Eigen/Core>       // To include it, set /usr/include/eigen3 to include-PATH (g++ -I/usr/include/eigen3).
+#include <Eigen/StdVector>
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Vector3d)
+
+#include "../include/gear_design/coordinates.hpp"
+
 
 /*---------------------------------------------
     naming convention
-    * _expected: the value expected to be
-    * _actual: the calculated actual value
+    * _expected: the value it is expected to be. it is difenitly ture.
+    * _actual: the calculated actual value. it could be false value.
 ----------------------------------------------*/
 
 TEST(SolutionTest, TransformGearBase) {
     /*
-    UNIT TEST
+    NAME CONVENTION:
+    the word "gearprofile" stands for "y-coordinate" at the coordinate.
+
+    This code tests the following operations.
+    0. At siggear coordinate, define siggear_x which are array of siggear_x.
+       This array is "expected_arr_siggear_x".
+    1. At siggear coordinate, calculate gearprofile from "expected_arr_siggear_x"
+       This array is "expected_arr_siggear_yprofile".
+    2. Transfer "expected_arr_siggear_x" to sigbase coordinate.
+       This array is "actual_arr_sigbase_x".
+    3. At sigbase coordinate, calculate gearprofile from actual_arr_sigbase_x.
+       This array is "actual_arr_sigbase_yprofile".
+    4. Check ASSERT expected_siggear_x == actual_sigbase_x
+       This confirms transformation from siggear to sigbase.
+    5. Check ASSERT expected_arr_siggear_yprofile == actual_arr_sigbase_yprofile
+       This confirms the same transformation. (optional test)
+    6. Transfer "actual_arr_sigbase_yprofile" to siggear
+       This array is "actual_arr_siggear_yprofile".
+    7. Check ASSERT expected_arr_siggear_yprofile == actual_arr_sigbase_yprofile
+       This confirms inverse transformation.
     */
 
-    /* set situation */
+    /* set basic parameters of the situation */
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis_r(20., 30.);
     double radius = dis_r(gen);
-    std::uniform_real_distribution<double> dis_theta(0., M_PI);
+    std::uniform_real_distribution<double> \
+        dis_theta(0., M_PI);     // not recommended (codes need to be changed)
+    std::uniform_real_distribution<double> \
+        dis_phi(-1. * M_PI / 2., M_PI / 2.);  // recommended
     double theta = dis_theta(gen);
-    // std::uniform_real_distribution<double> dis_phi(-1. * M_PI / 2., M_PI / 2.);
-    // double phi = dis_theta(gen);
+    double phi = dis_theta(gen);
+    const double N = 100.;  // the number of divisions on x-axis
+    const double siggear_x_range[2] = {-20., 20.};  // plot range of x-axis
 
-    /* set test condition */
-    const double N = 100.;  // N: the number of splitting
-    const double siggear_x_range[2] = {-20., 20.};
+    /* declare expected_ & actual_ variables */
+    std::vector<double> expected_arr_siggear_x(N, 0.);
+    std::vector<double> expected_arr_siggear_yprofile(N, 0.);
+    std::vector<double> expected_arr_siggear_Pvecprofile(N, Eigen::Vector3d::Zero());
+    std::vector<double> actual_arr_sigbase_x(N, 0.);
+    std::vector<double> actual_arr_sigbase_yprofile(N, 0.);
+    std::vector<double> actual_arr_sigbase_Pvecprofile(N, Eigen::Vector3d::Zero());
+    std::vector<double> actual_arr_siggear_x(N, 0.);
+    std::vector<double> actual_arr_siggear_yprofile(N, 0.);
+    std::vector<double> actual_arr_siggear_Pvecprofile(N, Eigen::Vector3d::Zero());
+
+    /* set expected_ variables */
     const double siggear_x_diff = (siggear_x_range[1] - siggear_x_range[0]) / N;
-    std::vector<double> gearprofile_siggear_x(N, 0.);
     for (int i = 0; i < N; ++i) {
-        gearprofile_siggear_x[i] = siggear_x_range[0] + siggear_x_diff * i;
+        expected_arr_siggear_x[i] = siggear_x_range[0] + siggear_x_diff * i;
+        expected_arr_siggear_yprofile[i] = \
+            gear_design::quad_calc_siggear_yprofile_from_siggear_x(expected_arr_siggear_x[i]);
+        expected_arr_siggear_Pvecprofile[i] << expected_arr_siggear_x[i],
+                                               expected_arr_siggear_yprofile[i],
+                                               1.;
     }
-    Eigen::Vector3d expected_gearprofile_siggear_Pvec = Eigen::Vector3d::Zero();
-    Eigen::Vector3d expected_gearprofile_sigbase_Pvec = Eigen::Vector3d::Zero();
 
-    /* test for each siggear_f_gearprofile_x */
+
+// today here 2024/11/20
+
+
+
+    /* set actual_ variables , transform from siggear to sigbase using non-Eigen function */
     for (int i = 0; i < N; ++i) {
-        double actual_gearprofile_sigbase_x = \
-            gear_design::calc_gearprofile_sigbase_x_from_siggear_x(siggear_gearprofile_x[i], radius, theta);
-        double actual_gearprofile_sigbase_y = \
-            gear_design::calc_gearprofile_sigbase_y_from_siggear_x(siggear_gearprofile_x[i], radius, theta);
+        actual_arr_sigbase_x = \
+            gear_design::calc_gearprofile_sigbase_x_from_siggear_x(
+                expected_arr_siggear_x[i], radius, theta
+                );
+        actual_arr_sigbase_yprofile = \
+            gear_design::calc_gearprofile_sigbase_y_from_siggear_x(
+                expected_arr_siggear_yprofile[i], radius, theta
+                );
+    }
 
-        /* To transform profile_vector from siggear to sigbase, use siggear_f_gearprofile_vector which uses Eigen */
-        gearprofile_siggear_Pvec << gearprofile_siggear_x[i],
-                                        gear_design::siggear_f_gearprofile(siggear_f_gearprofile_x[i]),
-                                        1.;
-        expected_gearprofile_sigbase_Pvec = \
+    /* set actual_ variables , transform from siggear to sigbase using Eigen function */
+    for (int i = 0; i < N; ++i) {
+        actual_arr_siggear_x << \
+            arr_gearprofile_siggear_x[i],\
+            gear_design::quad_profile_calc_siggear_y_from_siggear_x(siggear_f_gearprofile_x[i]),\
+            1.;
+        actual_arr_siggear_yprofile = \
             // gear_design::calc_gearprofile_sigbase_Pvec_from_siggear_Pvec(
             gear_design::trans_Pvec_from_siggear_to_sigbase(
                 expected_gearprofile_siggear_Pvec,
@@ -58,8 +109,8 @@ TEST(SolutionTest, TransformGearBase) {
                 theta);
 
         /* test */
-        ASSERT_NEAR(expected_gearprofile_sigbase_vec[0], actual_gearprofile_sigbase_x, 1e-6);
-        ASSERT_NEAR(expected_gearprofile_sigbase_vec[1], actual_gearprofile_sigbase_y, 1e-6);
+        ASSERT_NEAR(expected_arr_sigbase_vec[0], actual_arr_sigbase_x, 1e-6);
+        ASSERT_NEAR(expected_arr_sigbase_vec[1], actual_arr_sigbase_yprofile, 1e-6);
     }
 
 
